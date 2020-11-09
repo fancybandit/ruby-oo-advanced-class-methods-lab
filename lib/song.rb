a636f71c4696377edffeb658150eba7b63365e42
Jacob Klimenko <droidedjake@gmail.com>
2020-11-08 20:38:02 -0500

    commit tree 664fbcf79d31f84e79dddc5472b78e1a76ad315d

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 class Song
+  
   attr_accessor :name, :artist_name
   @@all = []
 
@@ -8,6 +9,57 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new_by_name(song_name)
+    song.save
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find {|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    matching = self.find_by_name(song_name)
+    if matching
+      matching
+    else
+      self.create_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    filename.slice!(".mp3")
+    artist_and_name = filename.split(" - ")
+    song = self.create_by_name(artist_and_name[1])
+    song.artist_name = artist_and_name[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    self.all << self.new_from_filename(filename)
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
